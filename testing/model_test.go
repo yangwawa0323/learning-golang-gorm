f0d16cd63c70e49b2f324770512763f3f9cba58d
yangwawa0323 <yangwawa0323@163.com>
2023-10-28 23:15:20 +0800

    first last take query

--- testing/model_test.go
+++ testing/model_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/jaswdr/faker"
 	"github.com/yangwawa0323/learning-golang-gorm/model"
 )
 
@@ -29,10 +30,12 @@ func testCreateUser(t *testing.T) {
 
 func testBatchCreateUsers(t *testing.T) {
 	var users []model.User = make([]model.User, 0)
+
+	p := faker.New().Person()
 	for i := 0; i < 100; i++ {
 		var user = model.User{
-			Name: fmt.Sprintf("JinZhu%03d", i),
-			Age:  18,
+			Name: p.Name(),
+			Age:  p.Faker.UInt8(),
 		}
 		users = append(users, user)
 	}
@@ -77,6 +80,57 @@ func testBatchCreateMapUsers(t *testing.T) {
 	t.Log("use map create user succeed")
 }
 
+func testDbFirst(t *testing.T) {
+	var user model.User
+	db.First(&user)
+	t.Logf("%#v", user)
+}
+
+func testDbTake(t *testing.T) {
+	var user model.User
+	db.Take(&user)
+	t.Logf("%#v", user)
+}
+
+func testDbLast(t *testing.T) {
+	var user model.User
+	db.Last(&user)
+	t.Logf("%v", user)
+}
+
+func testDbModelFirst(t *testing.T) {
+	result := map[string]interface{}{}
+	db.Model(&model.User{}).First(&result)
+
+	t.Logf("%#v", result)
+}
+
+func testDbModelTake(t *testing.T) {
+	result := map[string]interface{}{}
+	db.Model(&model.User{}).Take(&result)
+
+	t.Logf("%#v", result)
+}
+
+func testDbTableTake(t *testing.T) {
+	result := map[string]interface{}{}
+	query_res := db.Table("country").Take(&result)
+
+	if query_res.Error != nil {
+		t.Fail()
+	}
+	t.Logf("%#v", result)
+}
+
+func testDbTableFirst(t *testing.T) {
+	result := map[string]interface{}{}
+	query_res := db.Table("country").First(&result)
+	if query_res != nil {
+		t.Fail()
+	}
+	t.Logf("%#v", result)
+}
+
 // TestXXXX(t *testing.T)
 func TestModel(t *testing.T) {
 
@@ -93,7 +147,28 @@ func TestModel(t *testing.T) {
 	// t.Run("Use map to create user", testCreateMapUser)
 
 	// use map batch create two users
-	t.Run("Use map batch create two users", testBatchCreateMapUsers)
+	// t.Run("Use map batch create two users", testBatchCreateUsers)
+
+	// db.First   order by id ASC limit 1
+	// t.Run("db.First", testDbFirst)
+
+	// db.Take   limit 1
+	// t.Run("db.Take", testDbTake)
+
+	// db.Last  order by id DESC limit 1
+	//t.Run("db.Last", testDbLast)
+
+	// db.Model().First()
+	// t.Run("db.Model.First", testDbModelFirst)
+
+	// db.Model().Take()
+	// t.Run("db.Model.Take", testDbModelTake)
+
+	// db.Table().Take()
+	t.Run("db.Table.Take", testDbTableTake)
+
+	// db.Table().First()  👎
+	t.Run("db.Table.First", testDbTableFirst)
 }
 
 // Author 1-->N  Books
