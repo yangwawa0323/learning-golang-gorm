c205b7cee4e8afe9a9b5921158ad498a941dafde
yangwawa0323 <yangwawa0323@163.com>
2023-11-01 21:27:15 +0800

    modify the create 100 users with random birthday/age/email info

--- testing/model_test.go
+++ testing/model_test.go
@@ -3,10 +3,11 @@
 package testing
 
 import (
-	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 
+	"github.com/jaswdr/faker"
 	"github.com/yangwawa0323/learning-golang-gorm/model"
 )
 
@@ -28,17 +29,28 @@ func testCreateUser(t *testing.T) {
 }
 
 func testBatchCreateUsers(t *testing.T) {
+	fake := faker.New()
+	var (
+		max = 60
+		min = 20
+	)
+
 	var users []model.User = make([]model.User, 0)
 	for i := 0; i < 100; i++ {
+		var email = fake.Person().Contact().Email
+		var birth = fake.Time().TimeBetween(time.Now().AddDate(-50, 0, 0), time.Now().AddDate(-30, 0, 0))
 		var user = model.User{
-			Name: fmt.Sprintf("JinZhu%03d", i),
-			Age:  18,
+			Name:     fake.Person().Name(),
+			Age:      uint8(rand.Intn(max-min) + min),
+			Email:    &email,
+			Birthday: &birth,
 		}
+
 		users = append(users, user)
 	}
 
 	result := db.Create(&users)
-	t.Log(fmt.Sprintf("create %d users", result.RowsAffected))
+	t.Logf("create %d users", result.RowsAffected)
 
 }
 
@@ -87,13 +99,13 @@ func TestModel(t *testing.T) {
 	// t.Run("Create a user", testCreateUser)
 
 	// create 100 users
-	// t.Run("Batch create 100 users", testBatchCreateUsers)
+	t.Run("Batch create 100 users", testBatchCreateUsers)
 
 	// use map to create user
 	// t.Run("Use map to create user", testCreateMapUser)
 
 	// use map batch create two users
-	t.Run("Use map batch create two users", testBatchCreateMapUsers)
+	// t.Run("Use map batch create two users", testBatchCreateMapUsers)
 }
 
 // Author 1-->N  Books
